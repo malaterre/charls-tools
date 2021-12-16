69f1725db25ad9a8b97f60aafaf44f3954fefd99
Mathieu Malaterre <mathieu.malaterre@gmail.com>
2021-12-16 16:39:29 +0100

    add test

--- format.sh
+++ format.sh
@@ -2,4 +2,4 @@
 
 clang-format -style=file -i *.cpp
 clang-format -style=file -i *.h
-cmake-format -i CMakeLists.txt doc/CMakeLists.txt
+cmake-format -i CMakeLists.txt doc/CMakeLists.txt tests/CMakeLists.txt
