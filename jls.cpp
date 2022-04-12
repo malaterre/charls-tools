malaterre/charls-tools

commit f908a605fd7189b9c72e9a169cedf11c1ffecbdf

Fix interleave mode handling

jls.cpp
@@ -105,12 +105,18 @@ static std::vector<uint8_t> compress(image const& img, const jlst::jls_options& 
         if (options.interleave_mode != charls::interleave_mode::none)
             throw std::invalid_argument("Invalid interleave_mode for single component. Use 'none'");
     }
+    auto interleave_mode = img.get_image_info().interleave_mode();
+    if (options.has_interleave_mode)
+    {
+        // user wants to override default interleave mode:
+        interleave_mode = options.interleave_mode;
+    }
 
     charls::jpegls_encoder encoder;
 
     // setup encoder using compressor's options:
     encoder
-        .interleave_mode(options.interleave_mode)                   // interleave_mode
+        .interleave_mode(interleave_mode)                           // interleave_mode
         .near_lossless(options.near_lossless)                       // near_lossless
         .preset_coding_parameters(options.preset_coding_parameters) // preset_coding_parameters
         .color_transformation(options.color_transformation);        // color_transformation

cjpls_options.cpp
@@ -188,6 +188,7 @@ bool cjpls_options::process(int argc, char* argv[])
         }
         if (vm.count("interleave_mode"))
         {
+            jls_options_.has_interleave_mode = true;
             jls_options_.interleave_mode = string_to_interleave_mode(interleave_mode_str.c_str());
         }
         if (vm.count("preset_coding_parameters"))

cjpls_options.h
@@ -10,6 +10,7 @@
 namespace jlst {
 struct jls_options final
 {
+    bool has_interleave_mode{};
     charls::interleave_mode interleave_mode{};
     int near_lossless{};
     charls::jpegls_pc_parameters preset_coding_parameters{};

commit a535005b5c38efb2bf5b5a5215f0af1e5c10d4ee

Fix bug introduced in commit f908a60

jls.cpp
@@ -242,9 +242,9 @@ static std::vector<uint8_t> compress(image const& img, const jlst::jls_options& 
     (void)coltra_none;
 #endif
 
-    const auto transform_pixel_data{img.transform(options.interleave_mode)};
+    const auto transform_pixel_data{img.transform(interleave_mode)};
     size_t encoded_size;
-    if (options.interleave_mode == charls::interleave_mode::none)
+    if (interleave_mode == charls::interleave_mode::none)
     {
         encoded_size = encoder.encode(transform_pixel_data);
     }